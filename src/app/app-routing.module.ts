bbcf492a231067edf49866d3fdf4a15e917eeec7
Oisin Lynch <oisinlynch1999@hotmail.com>
2020-12-09 11:36:12 +0000

    Added auth guard in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './helpers/auth.guard';
+
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PlayerListComponent } from './player/player-list/player-list.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'players', component: PlayerListComponent},
+  {path: 'players', component: PlayerListComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent}
 ];
 
